32318bd50f8410fa93f93f0dd7ff343ce8618065
dos20 <dsepiolo@interia.pol>
2015-11-26 10:12:43 +0100

    nhbgv

---
+++ prog1.c
@@ -0,0 +1,14 @@
+// Pętla for
+
+#include <stdio.h>
+#include <conio.h>
+
+int main()
+{
+        int i;
+        printf("\n Cyfry w dziesietnym systemie liczbowym: \n");
+        for(i=0;i<10;i++)
+                printf(" %d ",i);
+        getch();
+        return 0;
+}
