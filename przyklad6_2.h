6f7319c961002ad377b257ceff69abf17e73e234
dos20 <dsepiolo@interia.pol>
2015-11-26 10:30:17 +0100

    12434564

---
+++ przyklad6_2.h
@@ -0,0 +1,14 @@
+// mój pierwszy string
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main ()
+{
+  string moj_ciag;
+  mystring = "To jest początkowa zawartość ciągu";
+  cout << moj_ciag << endl;
+  mystring = "To jest inna zawartość ciągu";
+  cout << moj_ciag << endl;
+  return 0;
+}
